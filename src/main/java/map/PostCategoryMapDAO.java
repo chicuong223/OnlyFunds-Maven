5706a9fbc5f82a5ec2075517089f9d8514a368ca
chiuycuong <77531153+chiuycuong@users.noreply.github.com>
2021-07-22 16:53:17 +0700

    Edit Category In Edit Post

--- src/main/java/map/PostCategoryMapDAO.java
+++ src/main/java/map/PostCategoryMapDAO.java
@@ -18,37 +18,51 @@ import utils.DBConnect;
  * @author chiuy
  */
 public class PostCategoryMapDAO {
-    public boolean addPostCatMap(User user, Category cat){
+
+    public boolean addPostCatMap(User user, Category cat) {
         boolean result = false;
-        try{
-            Connection con = DBConnect.makeConnection();
-            if(con != null){
+        try (Connection con = DBConnect.makeConnection()) {
+            if (con != null)
                 try (PreparedStatement ps = con.prepareStatement("INSERT INTO Post_Category_Map(post_id, category_id)\n"
-                        + "VALUES((SELECT MAX(id) FROM Post WHERE uploader_username = ?), ?)")) {
-                    ps.setString(1, user.getUsername());
-                    ps.setInt(2, cat.getCategoryId());
-                    result = ps.executeUpdate() > 0;
-                }
-                con.close();
+                    + "VALUES((SELECT MAX(id) FROM Post WHERE uploader_username = ?), ?)")) {
+                ps.setString(1, user.getUsername());
+                ps.setInt(2, cat.getCategoryId());
+                result = ps.executeUpdate() > 0;
             }
         }
-        catch (SQLException e){
+        catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return result;
     }
-    
-    public boolean deleteCategoryMapsByPost(Post post){
+
+    public boolean deleteCategoryMapsByPost(Post post) {
         boolean result = false;
-        try(Connection con = DBConnect.makeConnection()){
-            if(con != null){
-                try(PreparedStatement ps = con.prepareStatement("DELETE FROM Post_Category_Map WHERE post_id = ?")){
-                    ps.setInt(1, post.getPostId());
-                    result = ps.executeUpdate() > 0;
-                }
+        try (Connection con = DBConnect.makeConnection()) {
+            if (con != null)
+                try (PreparedStatement ps = con.prepareStatement("DELETE FROM Post_Category_Map WHERE post_id = ?")) {
+                ps.setInt(1, post.getPostId());
+                result = ps.executeUpdate() > 0;
             }
         }
-        catch (Exception e){
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public boolean addPostCatMapEdit(Post post, Category category) {
+        boolean result = false;
+        try (Connection con = DBConnect.makeConnection()) {
+            if (con != null)
+                try (PreparedStatement ps = con.prepareStatement("INSERT INTO Post_Category_Map(post_id, category_id)\n"
+                    + "VALUES(?, ?)")) {
+                ps.setInt(1, post.getPostId());
+                ps.setInt(2, category.getCategoryId());
+                result = ps.executeUpdate() > 0;
+            }
+        }
+        catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return result;
