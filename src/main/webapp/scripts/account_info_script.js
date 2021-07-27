2f868b92fff5e916cb6a87427a420ea246f807c6
chiuycuong <77531153+chiuycuong@users.noreply.github.com>
2021-07-27 10:29:16 +0700

    Manage account password

--- src/main/webapp/scripts/account_info_script.js
+++ src/main/webapp/scripts/account_info_script.js
@@ -1,50 +1,84 @@
-var change_password_form = document.querySelector("#change-password-form");
-var change_password_btn = document.querySelector("#change-password-btn");
-const PASSWORD_LENGTH_MIN = 8;
-const PASSWORD_LENGTH_MAX = 32;
 
-change_password_form.addEventListener("submit", event => {
-    
-    var curPassword = change_password_form.querySelector('input[name="currentPassword"]');
-    var newPassword = change_password_form.querySelector('input[name="newPassword"]');
-    var confNewPassword = change_password_form.querySelector('input[name="confNewPassword"]');
+//hidden field có tên là oldPass
+const oldPass = document.getElementById('oldPass');
+const oldPassin = document.getElementById('currPassword');
+const newPass = document.getElementById('newPassword');
+const confPass = document.getElementById('confPassword');
+const passRegex = /(?=.{6,})/;
 
-    if (curPassword.value.length < PASSWORD_LENGTH_MIN || curPassword.value.length > PASSWORD_LENGTH_MAX) {
-        console.log("password error");
+let oldPassErr = document.getElementById('passwordError');
+let newPassErr = document.getElementById('newPasswordError');
+let confPassErr = document.getElementById('confPasswordError');
+
+function validate(param) {
+    if (param === 'oldPass') {
+        console.log("Old password: " + oldPass.value);
+        console.log("Input old password: " + oldPassin);
+        console.log(sha256(oldPassin.value));
+        if (oldPass.value !== sha256(oldPassin.value)) {
+            oldPassErr.innerHTML = '<i class="fas fa-times"></i> Old password is incorrect, try again';
+            oldPassErr.style.display = "block";
+            oldPassErr.style.color = "red";
+            oldPassErr.className = "error";
+        } else {
+            oldPassErr.innerHTML = '<i class="fas fa-check"></i> Old password is correct';
+            oldPassErr.style.display = "block";
+            oldPassErr.style.color = "rgb(97 218 16)";
+            oldPassErr.className = "success";
+        }
+    } else if (param === 'newPass') {
+        if (passRegex.test(newPass.value) === false) {
+            newPassErr.innerHTML = '<i class="fas fa-times"></i> New password is invalid, try again';
+            newPassErr.style.display = "block";
+            newPassErr.style.color = "red";
+            newPassErr.className = "error";
+        } else {
+            newPassErr.innerHTML = '<i class="fas fa-check"></i> New password is valid';
+            newPassErr.style.display = "block";
+            newPassErr.style.color = "rgb(97 218 16)";
+            newPassErr.className = "success";
+        }
+        if (confPass.value !== newPass.value) {
+            confPassErr.innerHTML =
+                    '<i class="fas fa-times"></i> Password does not match';
+            confPassErr.style.display = "block";
+            confPassErr.className = "error";
+            confPassErr.style.color = "red";
+        } else {
+            confPassErr.innerHTML =
+                    '<i class="fas fa-check"></i> Password matched';
+            confPassErr.style.display = "block";
+            confPassErr.className = "success";
+            confPassErr.style.color = "rgb(97 218 16)";
+        }
+    } else if (param === 'confPass') {
+        if (confPass.value !== newPass.value) {
+            confPassErr.innerHTML =
+                    '<i class="fas fa-times"></i> Password does not match';
+            confPassErr.style.display = "block";
+            confPassErr.className = "error";
+            confPassErr.style.color = "red";
+        } else {
+            confPassErr.innerHTML =
+                    '<i class="fas fa-check"></i> Password matched';
+            confPassErr.style.display = "block";
+            confPassErr.className = "success";
+            confPassErr.style.color = "rgb(97 218 16)";
+        }
+    }
+}
+
+document.getElementById('change-password-form').addEventListener('submit', event => {
+    const isError = document.querySelectorAll(".error");
+    console.log(isError);
+    if (isError.length !== 0
+            || !oldPassin.value.trim()
+            || !newPass.value.trim()
+            || !confPass.value.trim()
+            || confPass.value.trim() !== newPass.value.trim()) {
         event.preventDefault();
-        document.getElementById('passwordError').textContent = "Password length must be between 8 to 32 characters";
-        return false;
-    }
-    else {
-        document.getElementById('passwordError').textContent = "";
-    }
-
-    if (newPassword.value.length < PASSWORD_LENGTH_MIN || newPassword.value.length > PASSWORD_LENGTH_MAX) {
-        console.log("password error");
-        event.preventDefault();
-        document.getElementById('newPasswordError').textContent = "Password length must be between 8 to 32 characters";
-        return false;
-    }
-    else {
-        document.getElementById('newPasswordError').textContent = "";
-    }
-
-    if (confNewPassword.value !== newPassword.value) {
-        console.log("confpass error");
-        event.preventDefault();
-        document.getElementById('confPasswordError').textContent = "Confirm password does not match";
-    }
-    else {
-        document.getElementById('confPasswordError').textContent = "";
+        alert('There is(are) invalid input(s), try again!');
+    } else {
+        alert("Change password successfully");
     }
 });
-
-let new_ava = document.getElementById('new-avatar');
-let img_ava = document.getElementById('img-avatar')
-new_ava.onchange = evt => {
-    const [file] = new_ava.files;
-    if (file) {
-        img_ava.src = URL.createObjectURL(file);
-    }
-    document.getElementById('update').classList.remove('disabled');
-}
